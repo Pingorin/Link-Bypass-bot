6fe8dc37040cffb6053f8fd9d08e768e748bd032
Pingorin <125970228+Pingorin@users.noreply.github.com>
2026-08-08 23:42:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,6 +3,6 @@ import os
 # Hugging Face Secrets se data lega. 
 # Agar aap local PC par test kar rahe hain, toh os.environ.get("API_ID", "APNA_ID_YAHAN_DALEIN") kar sakte hain.
 
-API_ID = os.environ.get("API_ID") 
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = os.environ.get("20638104") 
+API_HASH = os.environ.get("6c884690ca85d39a4c5ad7c15b194e42")
+BOT_TOKEN = os.environ.get("7718312636:AAG_cLNQRncu3LssN9UKe13ricM-8_fo4kU")
