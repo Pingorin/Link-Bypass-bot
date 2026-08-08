4c4e83be8855e2a757ac9d8d94ed894c3e081b3c
Pingorin <125970228+Pingorin@users.noreply.github.com>
2026-08-09 00:25:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,4 +5,4 @@ API_HASH = os.environ.get("API_HASH", "6c884690ca85d39a4c5ad7c15b194e42")
 
 # Token bilkul ek single line mein hona chahiye (yahan theek kar diya gaya hai)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7718312636:AAG_cLNQRncu3LssN9UKe13ricM-8_fo4kU")
-START_IMAGE = os.environ.get("START_IMAGE", "https://telegra.ph/file/4e8b3f2390a184e9185a7.jpg")
+START_IMAGE = os.environ.get("START_IMAGE", "https://graph.org/file/130d22d88710248c45b26.jpg")
